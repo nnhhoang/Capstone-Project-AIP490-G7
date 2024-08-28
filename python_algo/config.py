4ee407692005f7b290389a20b5bea408e2fe016e
quynam2205 <105327792+quynam2205@users.noreply.github.com>
2024-08-28 20:27:01 +0700

    Update config.py

--- python_algo/config.py
+++ python_algo/config.py
@@ -1,23 +1,10 @@
 from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
 from sentence_transformers import SentenceTransformer
 
-#GOOGLE_API_KEY='x'
 GOOGLE_API_KEY= 'x'
-# GOOGLE_API_KEY= 'x'
-# GOOGLE_API_KEY = 'x'
-# GOOGLE_API_KEY='x'
 
-GOOGLE_API_KEY_FOR_GEN = 'x'
 
-generation_config_gen = {
-        "temperature": 1,
-        "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
-        "response_mime_type": "text/plain"
-        }
-
-cache_time = 160
+cache_time = 30
 
 generation_config = {
         "temperature": 0,
@@ -35,10 +22,8 @@ safety_settings = {
 }
 
 MODEL_SEMANTIC = SentenceTransformer('all-MiniLM-L6-v2')
-# MODEL_SEMANTIC = SentenceTransformer("Alibaba-NLP/gte-large-en-v1.5", trust_remote_code=True)
-# MODEL_SEMANTIC = SentenceTransformer('microsoft/codebert-base')
 
-Test_set_path = r'data\question_data3.csv'
+Test_set_path = r'data\duplicate_matrix1.csv'
 
 cache_cost_per_token = 0.875/1000000
 cache_cost_per_hour = (4.5/1000000)/60
