cf7fd7fd208aa6b5989c8d96df6e66c40941428c
quynam2205 <105327792+quynam2205@users.noreply.github.com>
2024-08-28 19:16:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, send_from_directory, Response, jsonify
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory, Response, jsonify
 import pandas as pd
 import os
 import uuid
@@ -8,21 +8,27 @@ from python_algo.data_plot import data_analyze
 from python_algo.gemini_api import LLM
 import json
 import csv
+import numpy as np
+from python_algo.statistic import statistic
+import time
 
 global question_data, subchapter_spatial
-pdf_path = r"data\python_learn.pdf"
-database_path = r"data\question_data2.csv"
-new_question_path = r"C:\Users\namnq\Downloads\clean_question4.csv"
+pdf_path = r"current\input_file\pythonlearn.pdf"
+database_path = r"data\duplicate_matrix1.csv"
+new_question_path = r"data\question_data.csv"
+log_path = r"data\log_data\log_data2.json"
 
-
+#gemini_call = None
 gemini_call = LLM()
 transformer = SpatialTransform(pdf_path)
 question_database = question_database_manage(gemini_call, database_path)
-# plot_graph = data_analyze()
+plot_graph = data_analyze(database_path)
+report = statistic(log_path)
 
 
 question_data = pd.read_csv(new_question_path)
 subchapter_spatial = transformer.create_subchapter_matrix(pdf_path)
+
 
 app = Flask(__name__)
 
@@ -72,12 +78,13 @@ def upload_file():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
         file.save(filepath)
 
-        questions_datadata = []
+        questions_data = []
 
         # Đọc tệp CSV và lấy câu hỏi đầu tiên
-        with open(filepath, 'r') as f:
+        with open(filepath, 'r', encoding='utf-8') as f:
             reader = csv.reader(f)
             rows = [row for row in reader if any(row)]
+            
             for row in rows[1:]:
                 question_data = {
                     'id': row[0],
@@ -85,49 +92,72 @@ def upload_file():
                     'answer': row[2],
                     'difficulty': None,
                 }
-                questions_datadata.append(question_data)
+                questions_data.append(question_data)
 
-        return jsonify({'data':questions_datadata, 'success': True})
+        return jsonify({'data':questions_data, 'success': True})
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
     collected_data = request.form.get("collectedData")
-
+  
     #spatial transform from paragraph
     matrix = transformer.spatial_return(collected_data) 
   
     #extract table of content related
     table_content = transformer.get_subchapters_from_fragments(subchapter_spatial ,matrix)
     
-    # graph_data = plot_graph.plot(table_content)
+    graph_data = plot_graph.plot(table_content, matrix)
     filename = r"current\graph.json"
 
     # Open the file in write mode ('w') and save as JSON
-    # with open(filename, 'w', encoding='utf-8') as outfile:
-    #     json.dump(graph_data, outfile, ensure_ascii=False, indent=4)
-  
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json.dump(graph_data, outfile, ensure_ascii=False, indent=4)
+    return jsonify({'data': 'success'})
+    #return render_template('plot.html', question_content=collected_data)
+
+
+@app.route("/submitquestion", methods=["POST"])
+def submitquestion():
+    collected_data = request.form.get("collectedData")
     #extract question from upload file and compose information
-    question_id = json.loads(collected_data)["question"]
+    question_id = json.loads(collected_data)["id"]
+    # print("Hello", question_data)
     question_content = question_data[question_data["id"] == int(question_id)]
- 
-    instruc_prompt = gemini_call.get_prompt(task_num=1, question_1={"id": question_id, "question_content": question_content["question_format"].iloc[0], "ans": question_content["ANS"].iloc[0]}, question_2= None, iou=None)
-    instruction = gemini_call.get_completion(prompt=instruc_prompt)
+    #spatial transform from paragraph
+    matrix = transformer.spatial_return(collected_data) 
+  
+    #extract table of content related
+    table_content = transformer.get_subchapters_from_fragments(subchapter_spatial ,matrix)
     # instruction = None
+    instruc_prompt = gemini_call.get_prompt(1, {"id": question_id, "question_content": question_content["question_content"].iloc[0], "ans": question_content["ans"].iloc[0]}, None, collected_data)
+    
+    instruction = gemini_call.get_completion(instruc_prompt)
+    try:
+        instruction = json.loads(instruction)
+        instruction = instruction["Correct answer explanation"]
+    except json.JSONDecodeError as e:
+        instruction = instruction
     
     
-    question_content_add = {"id": question_id, "question_content": question_content["question_format"].iloc[0], "ans": question_content["ANS"].iloc[0], "difficulty": None,"subchapters": table_content, "paragraph": collected_data,"instruction": instruction,"spatial_matrix": matrix.tolist()}
+    question_content_add = {"id": question_id, "question_content": question_content["question_content"].iloc[0], "ans": question_content["ans"].iloc[0], "difficulty":  int(question_content['difficulty'].iloc[0]),"subchapters": table_content, "paragraph": collected_data,"instruction": instruction,"spatial_matrix": matrix.tolist()}
+    question_content_log = {"id": question_id, "question_content": question_content["question_content"].iloc[0], "ans": question_content["ans"].iloc[0], "difficulty": int(question_content['difficulty'].iloc[0]),"subchapters": table_content, "paragraph": collected_data,"instruction": instruction}
 
-    #question_database.add_question(question_content_add) 
-    #graph = ...
-    ranking_result, final_result = question_database.ranking_question(question_content_add)
-    print(ranking_result)
 
-    print(final_result)
+    ranking_result, answer = question_database.new_ranking_question(question_content_add)
     
+    question_compare = question_database.process_questions(question_content_add, database_path)
+
+    filename = r"current\graph.json"
+    with open(filename, 'r', encoding='utf-8') as infile:
+        graph_data =  json.load(infile)
     
-    
-    
-    return render_template('plot.html', data= ranking_result, question_content=collected_data)
+    log_data = {"id_log": None, "question_input": question_content_log, "ranking_result": ranking_result, 'explain': answer, 'graph': graph_data, 'question_compare': question_compare}
+    question_database.save_log(log_data)
+
+ 
+    return jsonify({'data': ranking_result, 'question_compare': question_compare, 'success': True})
+
 
 
 @app.route('/plot')
@@ -138,10 +168,21 @@ def index():
 def data():
     filename = r"current\graph.json"
     with open(filename, 'r', encoding='utf-8') as infile:
-            graph_data =  json.load(infile)
-    print(graph_data, 'to plot')
+        graph_data =  json.load(infile)
+  
     data = convert_data(graph_data)
+ 
     return jsonify(data)
+
+
+
+@app.route('/getLog', methods=["POST"])
+def getLog():
+    filename = r"data\log_data\log_data2.json"
+    with open(filename, 'r', encoding='utf-8') as infile:
+            data =  json.load(infile)
+    return jsonify({'data': data})
+
 
 def convert_data(input_data):
     output_data = {}
@@ -165,35 +206,94 @@ def convert_data(input_data):
         }
 
     
-    output_data['chart4'] = {
-        'spatial_match': ["90%", "80%", "70%", "60%"],
-        'number': [123, 456, 789, 0]
-    }
+    if 'four_graph' in input_data:
+        output_data['chart4'] = {
+            'spatial_match': input_data['four_graph'].get('spatial_match', []),
+            'number': input_data['four_graph'].get('number', [])
+        }
         
     return output_data
 
-# input_data = {
-#     "first_graph": {
-#         "learning_outcome": ["LO1", "LO2", "LO3"],
-#         "number": [123, 345, 456]
-#     },
-#     "second_graph": {
-#         "subchapter": ["4.1", "4.2", "4.3"],
-#         "number": [123, 345, 456]
-#     },
-#     "third_graph": {
-#         "subchapter": ["4.1", "4.2", "4.3"],
-#         "difficult_level": [
-#             [12, 13, 14],
-#             [12, 13, 14],
-#             [12, 13, 14]
-#         ]
-#     },
-#     "four_graph": {
-#         "spatial_match": ["90%", "80%", "70%", "60%"],
-#         "number": [123, 231, 356, 400]
-#     }
-# }
+
+@app.route('/getSubChapter', methods=['POST'])
+def getSubChapter():
+    data = {
+        "learning_outcome": ["LO1", "LO2", "LO3", "LO4", "LO5", "LO6", "LO7"],
+        "subchapters": [
+            ["1.2", "1.3", "1.4", "1.6"],             # LO1
+            ["2.2", "2.3", "2.4", "2.6"],             # LO2
+            ["3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7", "3.8", "3.9",  # LO3
+             "4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "4.10", "4.11", "4.12", 
+             "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7"], # LO3
+            ["6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "6.10", "6.11", "6.12"], # LO4
+            ["7.1", "7.2", "7.3", "7.4", "7.5", "7.6", "7.7", "7.8", "7.9"], # LO5
+            ["8.1", "8.2", "8.3", "8.4", "8.5", "8.6", "8.7", "8.8", "8.9", "8.10", "8.11", "8.12",  # LO6
+             "9.1", "9.2", "9.3", "9.4", "9.5", "10.1", "10.2", "10.3", "10.4"], 
+            ["14.1", "14.2", "14.3", "14.4", "14.5", "14.6", "14.7", "14.8", "14.9", "14.10", "14.11"] # LO7
+        ]
+    }
+    subchapters = data["subchapters"]
+    learning_outcome = data["learning_outcome"]
+    return jsonify({"subchapters": subchapters, "learning_outcome": learning_outcome})
+
+
+@app.route('/formatDataChart', methods=['POST'])
+def formatDataChart():
+    data = request.form.get("data")
+    print("raw",data)
+    print("convert",log_convert_data(data))
+    return jsonify(log_convert_data(data))
+
+def log_convert_data(input_data):
+    output_data = {}
+    input_data = json.loads(input_data)
+    if 'first_graph' in input_data:
+        output_data['chart1'] = {
+            'learning_outcome': input_data['first_graph'].get('learning_outcome', []),
+            'number': input_data['first_graph'].get('number', [])
+        }
+
+    if 'second_graph' in input_data:
+        output_data['chart2'] = {
+            'subchapter': input_data['second_graph'].get('subchapter', []),
+            'number': input_data['second_graph'].get('number', [])
+        }
+
+    if 'third_graph' in input_data:
+        output_data['chart3'] = {
+            'subchapter': input_data['third_graph'].get('subchapter', []),
+            'difficult_level': input_data['third_graph'].get('number', [])
+        }
+
+    if 'four_graph' in input_data:
+        output_data['chart4'] = {
+            'spatial_match': input_data['four_graph'].get('spatial_match', []),
+            'number': input_data['four_graph'].get('number', [])
+        }
+    return output_data
+
+@app.route('/save_question', methods=['POST'])
+def save_question():
+    try:
+        data = request.get_json()  # Nhận dữ liệu JSON từ request
+        new_question = data.get('new_question')
+
+        # Lưu dữ liệu vào file CSV
+        with open('data/questions.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'question_content', 'ans', 'difficulty', 'learning_outcome', 
+                          'subchapters', 'paragraph', 'instruction', 'spatial_matrix']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # Kiểm tra xem file CSV có trống không
+            # Nếu trống thì ghi header
+            if csvfile.tell() == 0:
+                writer.writeheader()
+            writer.writerow(new_question)  
+
+        return jsonify({'message': 'Success!'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- eval_main.py
+++ eval_main.py
@@ -2,13 +2,13 @@ import pandas as pd
 from python_algo.gemini_api import LLM
 from python_algo.evaluation import cost_in_out, total_cost, eval_instruction
 from python_algo.database_management import question_database_manage
-from python_algo.semantic_process import Semantic_eval
+from python_algo.semantic import Semantic_eval
 import json
 
 
-data_in_path = r"data\question_data3.csv"
+data_in_path = r"data\question_data.csv"
 database_path = r"data\question_data_full.csv"
-data_in = pd.read_csv(r'data\question_data3.csv')
+data_in = pd.read_csv(r'data\question_data.csv')
 data_base = pd.read_csv(r"data\question_data_full.csv")
 Gemini = LLM()
 question_database = question_database_manage(Gemini, database_path)
@@ -23,19 +23,19 @@ data_dup = []
 list_ins = []
 
 for row in data_in.itertuples():
-    # prompt_ins = Gemini.get_prompt(1, question_1={"id": row.id, "question_content": row.question_content, "ans": row.ans}, question_2=None)    
-    # result, metadata = Gemini.get_completion_for_eval(prompt=prompt_ins)
-    # cost += cost_in_out(metadata=metadata)
-    # metadata_gemini = metadata
-    # print(result, metadata, type(result))
+    prompt_ins = Gemini.get_prompt(1, question_1={"id": row.id, "question_content": row.question_content, "ans": row.ans}, question_2=None)    
+    result, metadata = Gemini.get_completion_for_eval(prompt=prompt_ins)
+    cost += cost_in_out(metadata=metadata)
+    metadata_gemini = metadata
+    print(result, metadata, type(result))
     
-    # try:
-    #     result= json.loads(result)
-    #     instruction = result["Correct answer explanation"]
-    # except json.JSONDecodeError as e:
-    #     print(f"Error decoding JSON: {e}")
+    try:
+        result= json.loads(result)
+        instruction = result["Correct answer explanation"]
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
     
-    # list_ins.append(result)
+    list_ins.append(result)
     
     question_main = {"id": row.id, 
                      "question_content": row.question_content, 
@@ -46,13 +46,13 @@ for row in data_in.itertuples():
                      "paragraph": row.paragraph,
                      "spatial_matrix": json.loads(row.spatial_matrix)}
     
-    ranking_result, final_result, metadata = question_database.new_ranking_question(question_main,k=10)
-    # cost += cost_in_out(metadata=metadata)
-    # ranking_result = sorted(ranking_result, key=lambda x: (x[3], x[1], x[2]), reverse=True)
-    # print(ranking_result, metadata)
-    # real_dup, out_dup = eval_instruction(row.id, ranking_result)
-    # data_dup.append([row.id, real_dup, out_dup])
-    # list_rank[row.id]=ranking_result
+    ranking_result, final_result, metadata = question_database.new_ranking_question(question_main,k=len(data_base))
+    cost += cost_in_out(metadata=metadata)
+    ranking_result = sorted(ranking_result, key=lambda x: (x[3], x[1], x[2]), reverse=True)
+    print(ranking_result, metadata)
+    real_dup, out_dup = eval_instruction(row.id, ranking_result)
+    data_dup.append([row.id, real_dup, out_dup])
+    list_rank[row.id]=ranking_result
 
     # count+=1
     # if count==1: 
@@ -61,10 +61,6 @@ for row in data_in.itertuples():
 # prompt_eval = Gemini.get_prompt(3,question_1=data_in, question_2=list_ins)
 # ins_result, metadata = Gemini.get_completion_for_eval(prompt=prompt_eval)
 # semantic_eval = Semantic_eval(list_ins)
-
-index = ['id', 'real_dup', 'out_dup']
-data_dup = pd.DataFrame(data_dup, columns=index)
-data_dup.to_csv('data\dup_result.csv', index=False)
 
 # Serializing json
 json_object = json.dumps(list_rank)
